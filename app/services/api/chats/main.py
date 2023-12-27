db0f9f8be4e25978c4aed8314faa7e8194a53412
Reginald Osano <rgo02439@gmail.com>
2023-12-28 03:22:36 +0800

    Adjusted alignment

--- app/services/api/chats/main.py
+++ app/services/api/chats/main.py
@@ -13,24 +13,24 @@ class ChatQuery(BaseModel):
 class ChatService():
 
 	def get_ai_generated_answer(self, question: str):
-			question_template_text = "{request}/n{format_instructions}"
+		question_template_text = "{request}/n{format_instructions}"
 
-			pydantic_parser = pydantic_parsers.get_pydantic_output_parser(pydantic_object=ChatQuery)
+		pydantic_parser = pydantic_parsers.get_pydantic_output_parser(pydantic_object=ChatQuery)
 
-			human_prompt = human.get_human_prompt(question_template_text)
+		human_prompt = human.get_human_prompt(question_template_text)
 
-			system_prompt = system.get_system_prompt(general_knowledge_system_template)
+		system_prompt = system.get_system_prompt(general_knowledge_system_template)
 
-			chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt])
+		chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt])
 
-			model_request = chat_prompt.format_prompt(
-				request=question,
-				format_instructions=pydantic_parser.get_format_instructions()
-			).to_messages()
+		model_request = chat_prompt.format_prompt(
+			request=question,
+			format_instructions=pydantic_parser.get_format_instructions()
+		).to_messages()
 
-			data = model(model_request, temperature=0)
+		data = model(model_request, temperature=0)
 
-			return data
+		return data
 
 			
 	
