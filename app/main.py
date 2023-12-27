be129d1c1cbe7c594ec1b69e9c390e00c11bbad0
Reginald Osano <rgo02439@gmail.com>
2023-12-27 09:08:44 +0800

    latest codes

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to FastAPIs!"}
+    return {"message": "Welcome to Lang Chain APIs!"}
+
+@app.get("/favicon.ico")
+def favicon():
+    return {"message": "Regie's Favicon!"}
+
 
 app.include_router(chat.router)
 
